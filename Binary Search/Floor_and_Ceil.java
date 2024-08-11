b035bce56b1d8a32f19c4f3685f10fc16e35f40f
aggarwalarshia <160538181+aggarwalarshia@users.noreply.github.com>
2024-08-11 14:52:21 +0530

    Binary Search

---
+++ Binary Search/Floor_and_Ceil.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Floor_and_Ceil {
+    public static int[] getFloorAndCeil(int[] arr, int n, int x) {
+        // Wriute your code here.
+        int[] ans = new int[2];
+        ans[0] = -1;
+        ans[1] = -1;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] <= x) {
+                ans[0] = arr[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        low = 0;
+        high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x) {
+                ans[1] = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
